4be496cab9ec0bafcd42d37a5287397e02cd2149
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-10-30 14:41:18 +0900

    Feat: Player condition UI

---
+++ Assets/Scripts/PlayerCondition.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+public interface IDamagable
+{
+    void TakePhysicalDamage(int damage);
+}
+
+public class PlayerCondition : MonoBehaviour, IDamagable
+{
+    public UIConditions uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition hunger { get { return uiCondition.hunger; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (hunger.curValue <= 0.0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if (health.curValue <= 0.0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float value)
+    {
+        health.Add(value);
+    }
+
+    public void Eat(float value)
+    {
+        hunger.Add(value);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Die");
+    }
+
+    public void TakePhysicalDamage(int damage)
+    {
+        health.Subtract(damage);
+        onTakeDamage?.Invoke();
+    }
+}
